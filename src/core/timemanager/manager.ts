add26540cd2486701ab70d4825cc25c93531a37e
Melody Daniel <melodyogonna@gmail.com>
2023-05-23 13:48:44 +0100

    start working on time management

---
+++ src/core/timemanager/manager.ts
@@ -0,0 +1,7 @@
+export class TimeManager {
+  private excluded = [23, 0, 1, 2, 3, 4, 5];
+  private morning = [6, 7, 8];
+  private afternoon = [14, 15];
+  private evening = [17, 18, 19];
+  private night = [20, 21, 22];
+}
